fd3748befaa4b1a01b5cffcdc611faf7d7140f53
Motyak <anfossotommy@hotmail.fr>
2020-10-04 02:27:14 +0200

    exo5 marche pas

--- src/exo5.cpp
+++ src/exo5.cpp
@@ -7,30 +7,36 @@
 const int BARRAGE_REMPLI = 5;
 
 // faire monter l'eau
-void up(int* value, std::mutex* m, std::condition_variable* cond_var)
+void up(int* value, std::mutex* m, std::condition_variable* cond_var, bool* trigger)
 {
-    // std::lock_guard<std::mutex> lcm(*m);
     while(true)
     {
         std::unique_lock<std::mutex> lck(*m);
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));//
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
         *value += 1;
         std::cout<<"quantité d'eau = "<<*value<<std::endl;
-        cond_var->notify_one();
-        cond_var->wait(lck, [value]{return *value == BARRAGE_REMPLI;});
+        if(*value == 5)
+        {
+            *trigger = true;
+            cond_var->notify_one();
+            cond_var->wait(lck, [trigger]{return !*trigger;});
+        }
+        // cond_var->notify_one();
+        // cond_var->wait(lck, [value]{return *value == BARRAGE_REMPLI;});
     }
 }
 
 // faire baisser l'eau
-void down(int* value, std::mutex* m, std::condition_variable* cond_var)
+void down(int* value, std::mutex* m, std::condition_variable* cond_var, bool* trigger)
 {
     while(true)
     {
         std::unique_lock<std::mutex> lck(*m);
-        cond_var->wait(lck, [value]{return *value == BARRAGE_REMPLI;});
+        cond_var->wait(lck, [trigger]{return *trigger;});
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));//
         *value = 0;
         std::cout<<"quantité d'eau = "<<*value<<std::endl;
+        *trigger = false;
         cond_var->notify_one();
     }
 }
@@ -38,16 +44,13 @@ void down(int* value, std::mutex* m, std::condition_variable* cond_var)
 // g++ -o bin/exo5 src/exo5.cpp --std=c++11 -lpthread
 int main()
 {
-    int* value = new int(0);
-    std::mutex* m = new std::mutex();
-    std::condition_variable* cond_var = new std::condition_variable();
+    std::mutex m;
+    std::condition_variable cond_var;
+    int value = 0;
+    bool trigger = false;
 
-    std::thread t1(up, value, m, cond_var);
-    std::thread t2(down, value, m, cond_var);
-
-    delete(value);
-    delete(m);
-    delete(cond_var);
+    std::thread t1(up, &value, &m, &cond_var, &trigger);
+    std::thread t2(down, &value, &m, &cond_var, &trigger);
 
 	return 0;
 }
